9220df94084787d86beff81d9142ea5c11b9117e
bmonette <benoitmonette80@gmail.com>
2025-05-09 23:06:05 -0400

    Created main page components

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <main>
+      <h2>Main Content</h2>
+      <p>This is the main area of the website.</p>
+    </main>
+  );
+}
+
+export default Main;
