e53b161f43243733557c381d6addd8e875639fdf
bmonette <benoitmonette80@gmail.com>
2025-05-09 23:57:50 -0400

    Added Nav component and styled it.

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import { Link } from "react-router-dom";
+import "./Nav.css";
 
 function Nav() {
   return (
     <nav>
-      <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
+      <div className="container header-container">
+        <img src="/assets/logo2.png" alt="Little Lemon Logo" className="logo" />
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/menu">Menu</Link></li>
+          <li><Link to="/booking">Reservations</Link></li>
+        </ul>
+      </div>
     </nav>
   );
 }
